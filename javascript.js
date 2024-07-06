189401dbf3bfbb02773ed9a99e0986b8f4068472
RaquelGuzman91 <rakelguzman91@gmail.com>
2024-07-06 13:52:06 -0600

    negocio corporativo

---
+++ javascript.js
@@ -0,0 +1,53 @@
+/*cambiar el icono por cerrar*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bi-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*================   quita el menu al hacer scroll   ===============*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+        /*===================        despliega el menu       ==================*/
+        let header = document.querySelector('header');
+        /*==================    despliega el nav con menu   ===============*/
+        header.classList.toggle('sticky', window.scrollY > 100);
+        /*=================== remueve el menu al dar clic =============*/
+        menuIcon.classList.remove('bi-x');
+        navbar.classList.remove('active');
+
+};
+
+/*====================================*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.container-banner h2, .img-contacto img', { origin: 'left'});
+ScrollReveal().reveal('.container-banner p, .features .info, .contacto form', { origin: 'rigth'});
+ScrollReveal().reveal('.container-features', { origin: 'top'});
+
+
+
